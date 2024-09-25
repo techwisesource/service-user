6f7c6ae0839ad14f7b812d4d49f76a31027fd443
aryandh.a.w <aryandh.a.w@gmail.com>
2024-09-25 14:39:32 +0700

    create endpoint get refresh_token

--- routes/refreshTokens.js
+++ routes/refreshTokens.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const refreshTokenHandler = require("./handler/refresh-token");
 
 router.post("/", refreshTokenHandler.create);
+router.get("/", refreshTokenHandler.getToken);
 
 module.exports = router;
