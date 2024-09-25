b20d9e4000b58daeb867de10148ad14465da113f
aryandh.a.w <aryandh.a.w@gmail.com>
2024-09-25 11:16:33 +0700

    create params user_ids

--- routes/handler/users/getUsers.js
+++ routes/handler/users/getUsers.js
@@ -1,9 +1,21 @@
 const { User } = require("../../../models");
 
 module.exports = async(req, res) => {
-    const users = await User.findAll({
+
+    // default userIds is empty array
+    const userIds = req.query.user_ids || [];
+
+    const sqlOption = {
         attributes: ["id", "name", "email", "profession", "role", "avatar"]
-    });
+    };
+
+    if(userIds.length) {
+        sqlOption.where = {
+            id: userIds
+        }
+    }
+
+    const users = await User.findAll(sqlOption);
 
     if(!users) {
         return res.status(404).json({
